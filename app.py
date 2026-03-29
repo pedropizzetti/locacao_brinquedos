41a9ec93fb085c3619279e3142c7be30c631e890
Pedro Pizzetti <pedrohpizzetti@gmail.com>
2026-03-29 12:53:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def conectar():
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
         user=st.secrets["mysql"]["user"],
-        password=st.secrets["mysql"]["password"]
+        password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"]
     )
 
