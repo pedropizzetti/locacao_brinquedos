4daa7a78bf95c71c0ebc1243a1d9163724e9f830
Pedro Pizzetti <pedrohpizzetti@gmail.com>
2026-03-29 17:04:00 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,27 +1,31 @@
-import streamlit as st
-
 def login():
+    import streamlit as st
+
     if 'logado' not in st.session_state:
         st.session_state['logado'] = False
 
-    if not st.session_state['logado']:
-        st.markdown("<h2 style='text-align: center;'>Sistema Mais Brinquedos</h2>", unsafe_allow_html=True)
+    if st.session_state['logado']:
+        return True
 
-        col1, col2, col3 = st.columns([2, 1, 2])
+    st.markdown("<h2 style='text-align: center;'>Sistema Mais Brinquedos</h2>", unsafe_allow_html=True)
 
-        with col2:
-            st.markdown("### Login")
+    col1, col2, col3 = st.columns([2, 1, 2])
 
-            with st.form("login_form"):
-                u_raw = st.text_input("Usuário").strip().lower()
-                p_raw = st.text_input("Senha", type="password")
+    with col2:
+        st.markdown("### Login")
 
-                btn_login = st.form_submit_button("Entrar", use_container_width=True)
+        with st.form("login_form"):
+            u_raw = st.text_input("Usuário").strip().lower()
+            p_raw = st.text_input("Senha", type="password")
 
-                if btn_login:
-                    if u_raw in st.secrets["usuarios"] and str(st.secrets["usuarios"][u_raw]) == p_raw:
-                        st.session_state['logado'] = True
-                        st.session_state['usuario_nome'] = u_raw
-                        st.rerun()
-                    else:
-                        st.error("Usuário ou senha incorretos.")
+            btn_login = st.form_submit_button("Entrar", use_container_width=True)
+
+            if btn_login:
+                if u_raw in st.secrets["usuarios"] and str(st.secrets["usuarios"][u_raw]) == p_raw:
+                    st.session_state['logado'] = True
+                    st.session_state['usuario_nome'] = u_raw
+                    st.rerun()
+                else:
+                    st.error("Usuário ou senha incorretos.")
+
+    return False
